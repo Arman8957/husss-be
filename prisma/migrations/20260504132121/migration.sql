2a8002acf4b94982464f5fa27535356cd24580a6
Arman8957 <novicethought0@gmail.com>
2026-05-04 19:22:39 +0600

    added BFRSessionCategory

---
+++ prisma/migrations/20260504132121/migration.sql
@@ -0,0 +1,14 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "BFRSessionCategory" ADD VALUE 'CONDITIONING';
+ALTER TYPE "BFRSessionCategory" ADD VALUE 'REHAB_STYLE';
+ALTER TYPE "BFRSessionCategory" ADD VALUE 'DELOAD';
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "trialEndsAt" SET DEFAULT now() + interval '7 days';
